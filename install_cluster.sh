9c0aad491f99e32b76f48606dd9987f1d129542f
Deepak S <deepak.santhosh@oneconvergence.com>
2019-12-05 13:05:41 +0530

    df

--- install_cluster.sh
+++ install_cluster.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+host=$1
+
 sudo swapoff -a
-sudo kubeadm init --kubernetes-version 1.12.5 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all
+if [[ -z $host ]]; then
+    sudo kubeadm init --kubernetes-version 1.15 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all
+else
+    sudo kubeadm init --kubernetes-version 1.15 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all --apiserver-advertise-address=$host
+fi
 mkdir -p $HOME/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -10,3 +16,4 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 sudo kubeadm token create --print-join-command
+
