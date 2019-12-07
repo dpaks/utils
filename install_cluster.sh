f7bfa98637fda951e4536aeb8278f8971088ca21
Deepak S <in.live.in@live.in>
2019-12-07 15:36:15 +0530

    Update install_cluster.sh

--- install_cluster.sh
+++ install_cluster.sh
@@ -12,7 +12,7 @@ mkdir -p $HOME/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 sudo kubeadm token create --print-join-command
