f69c1f54081ee57fa8cdb332171dcbc5d397f1b9
Deepak S <in.live.in@live.in>
2020-10-08 13:55:55 +0530

    upped to 1.18.9

--- install_cluster.sh
+++ install_cluster.sh
@@ -4,15 +4,15 @@ host=$1
 
 sudo swapoff -a
 if [[ -z $host ]]; then
-    sudo kubeadm init --kubernetes-version 1.15.6 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all
+    sudo kubeadm init --kubernetes-version 1.18.9 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all
 else
-    sudo kubeadm init --kubernetes-version 1.15.6 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all --apiserver-cert-extra-sans=$host
+    sudo kubeadm init --kubernetes-version 1.18.9 --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all --apiserver-cert-extra-sans=$host
 fi
 mkdir -p $HOME/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.12.0/Documentation/kube-flannel.yml
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 sudo kubeadm token create --print-join-command
